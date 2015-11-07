2d172f3fa931bd92ca1f87ad0bf447c586bd191b
lanrutcon <x0flip0x@hotmail.com>
2015-11-07 18:29:25 +0000

    Added Anchor and Commands
    Now all icons should be pop up on the anchor and grow to the left.
    Also, added slash commands for unlock/lock anchor.

--- DevouredByVermin.lua
+++ DevouredByVermin.lua
@@ -1,8 +1,10 @@
 local Addon = CreateFrame("FRAME", "BloodwormTracker");
 
 local wormsTable = {};
+local wormsTableSize = 0;
 local bloodwormsHP = UnitHealthMax("player")*0.35;
 
+local frameAnchor = nil;
 
 
 local function getAmountHeal(bloodwormGUID)
@@ -10,59 +12,113 @@ local function getAmountHeal(bloodwormGUID)
 end
 
 
-local function createFrameIcon()
+
+local function createFrameIcon(index)
 	local frame = CreateFrame("FRAME", nil, UIParent);
 	frame:SetSize(32, 32);
-	frame:SetPoint("CENTER", UIParent, "CENTER");
-    
-    frame.icon = frame:CreateTexture("IconTexture", "BACKGROUND")
-    frame.icon:SetWidth(32)
-    frame.icon:SetHeight(32)
-    frame.icon:SetPoint("TOPLEFT", 0, 0)
-    frame.icon:SetTexture("Interface\\ICONS\\Spell_DeathKnight_BloodBoil.png");
-    
-    frame.stack = frame:CreateFontString("IconStack", "OVERLAY", "GameFontNormal")
-    frame.stack:SetFont("Fonts\\FRIZQT__.TTF", 16, "OUTLINE");
-    frame.stack:SetPoint("CENTER", 0, 0)
-    
-    frame:EnableMouse(true)
-    frame:SetMovable(true);
-    
-    frame:SetScript("OnMouseDown", function(self, button)
-    	if(IsAltKeyDown() and button == "LeftButton") then
-    		self:StartMoving();
-    	end
-    end)
-    frame:SetScript("OnMouseUp", function(self, button)
+	frame:SetPoint("LEFT", frameAnchor, index*32, 0);
+
+	frame.icon = frame:CreateTexture("IconTexture", "BACKGROUND")
+	frame.icon:SetWidth(32)
+	frame.icon:SetHeight(32)
+	frame.icon:SetPoint("TOPLEFT", 0, 0)
+	frame.icon:SetTexture("Interface\\ICONS\\Spell_DeathKnight_BloodBoil.png");
+
+	frame.stack = frame:CreateFontString("IconStack", "OVERLAY", "GameFontNormal")
+	frame.stack:SetFont("Fonts\\FRIZQT__.TTF", 16, "OUTLINE");
+	frame.stack:SetPoint("CENTER", 0, 0)
+
+	frame:Show();
+
+	return frame;
+end
+
+local function initFrameAnchor()
+	frameAnchor = CreateFrame("FRAME", "DBVanchor", UIParent);
+	frameAnchor:SetSize(32, 32);
+	frameAnchor:SetPoint("CENTER", UIParent, "CENTER");
+	
+	frameAnchor.icon = frameAnchor:CreateTexture("IconTexture", "BACKGROUND");
+	frameAnchor.icon:SetWidth(32)
+	frameAnchor.icon:SetHeight(32)
+	frameAnchor.icon:SetPoint("TOPLEFT", 0, 0)
+	frameAnchor.icon:SetTexture("Interface\\ICONS\\Spell_DeathKnight_BloodBoil.png");
+
+
+	frameAnchor:EnableMouse(true)
+	frameAnchor:SetMovable(true);
+	
+	
+	frameAnchor:SetScript("OnMouseDown", function(self, button)
+		if(button == "LeftButton") then
+			self:StartMoving();
+		end
+	end)
+	frameAnchor:SetScript("OnMouseUp", function(self, button)
 		self:StopMovingOrSizing();
-    end)
-    
-    frame:Show();
-    
-    return frame;
+		local point,_,relativePoint,x,y = self:GetPoint();
+		DevouredByVerminSV[UnitName("player")] = { point, relativePoint, x, y };
+	end)
+	
+	frameAnchor:Hide();
 end
 
 
-Addon:SetScript("OnEvent", function(self, event, ...)
-	--theres no need to check the event, because there is only one to track
-	local time, type, _, sourceGUID, sourceName, _, _, destGUID, destName, _, _, spellID, spellName, _, auraType, numStack = ...;
-	if(type == "SPELL_SUMMON" and sourceName == UnitName("player") and destName == "Bloodworm") then
-		destGUID = tostring(destGUID);
-		print("A bloodworm has poped up");
-		local frame = createFrameIcon();
-		wormsTable[destGUID] = { stack = 0, frame = frame };
-	elseif(type == "SPELL_AURA_APPLIED_DOSE" and sourceName == "Bloodworm" and spellID == 81277) then
-		destGUID = tostring(destGUID);
-		wormsTable[destGUID]["stack"] = numStack;
-		wormsTable[destGUID]["frame"].stack:SetText(numStack);
-		print("The stack is " .. numStack);
-	elseif(type == "SPELL_AURA_REMOVED" and sourceName == "Bloodworm" and spellID == 81277) then
-		destGUID = tostring(destGUID);
-		wormsTable[destGUID]["frame"]:Hide();
-		wormsTable[destGUID] = nil;
-		print("PUFF, Mega-Heal");
+
+SLASH_DevouredByVermin1, SLASH_DevouredByVermin2 = "/devouredbyvermin", "/dbv";
+
+function SlashCmd(cmd)
+	if (cmd:match"unlock") then
+		print("unlock")
+		frameAnchor:Show();
+	elseif (cmd:match"lock") then
+		frameAnchor:Hide();
 	end
+end
+
+SlashCmdList["DevouredByVermin"] = SlashCmd;
+
+Addon:SetScript("OnEvent", function(self, event, ...)
+
+		if(event == "VARIABLES_LOADED") then
+			initFrameAnchor();
+			if type(DevouredByVerminSV) ~= "table" then
+				DevouredByVerminSV = {};
+				local point, relativePoint, x, y = frameAnchor:GetPoint();
+				DevouredByVerminSV[UnitName("player")] = { point, relativePoint, x, y };
+			elseif(DevouredByVerminSV[UnitName("player")]) then
+				local point, relativePoint, x, y = DevouredByVerminSV[UnitName("player")][1], DevouredByVerminSV[UnitName("player")][2], DevouredByVerminSV[UnitName("player")][3], DevouredByVerminSV[UnitName("player")][4];
+				print(point, relativePoint, x, y);
+				frameAnchor:SetPoint(point, UIParent, relativePoint, x, y);
+			else
+				local point, relativePoint, x, y = frameAnchor:GetPoint();
+				DevouredByVerminSV[UnitName("player")] = { point, relativePoint, x, y };
+			end
+		else
+			local time, type, _, sourceGUID, sourceName, _, _, destGUID, destName, _, _, spellID, spellName, _, auraType, numStack = ...;
+			if(type == "SPELL_SUMMON" and sourceName == UnitName("player") and destName == "Bloodworm") then
+				destGUID = tostring(destGUID);
+				print("A bloodworm has poped up");
+				local frame = createFrameIcon(wormsTableSize);
+				wormsTable[destGUID] = { stack = 0, frame = frame };
+				wormsTableSize = wormsTableSize + 1;
+			elseif(type == "SPELL_AURA_APPLIED_DOSE" and sourceName == "Bloodworm" and spellID == 81277) then
+				destGUID = tostring(destGUID);
+				wormsTable[destGUID]["stack"] = numStack;
+				wormsTable[destGUID]["frame"].stack:SetText(numStack);
+				print("The stack is " .. numStack);
+			elseif(type == "SPELL_AURA_REMOVED" and sourceName == "Bloodworm" and spellID == 81277) then
+				destGUID = tostring(destGUID);
+				wormsTable[destGUID]["frame"]:Hide();
+				wormsTable[destGUID] = nil;
+				print("PUFF, Mega-Heal");
+				wormsTableSize = wormsTableSize - 1;
+			end
+		end
 end)
 
-Addon:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+
+
+Addon:RegisterEvent("VARIABLES_LOADED");
+Addon:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
 
